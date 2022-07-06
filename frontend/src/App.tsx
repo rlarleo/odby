fedef0e79e69fb1f1de8999285cd6b11baf2f373
Kidae-Kim <Kidae.kim@daydreamlab.co.kr>
2022-07-06 16:32:11 +0900

    aos library 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,6 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 import HeroBanner from './section/HeroBanner';
 import IntroSection from './section/IntroSection';
 import VideoSection from './section/VideoSection';
@@ -8,6 +11,13 @@ import Axios from 'axios';
 Axios.defaults.baseURL = process.env.REACT_APP_API_SERVER;
 console.log(process.env.REACT_APP_API_SERVER)
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+    });
+  }, []);
+
   return (
     <div className="App">
       <HeroBanner />
--- frontend/src/section/IntroSection.tsx
+++ frontend/src/section/IntroSection.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import FadeIn from '../component/Fade/FadeIn'; // Import react-reveal(Fade)
+import { Box, Stack } from '@mui/material';
 
 const IntroSection: React.FC = () => {
   const image = `${process.env.PUBLIC_URL}/images/OddLogo.png`
   return (
     <div className="App-section">
-      <FadeIn> 
-        <img src={image} className="App-logo" alt="logo" />
-        ODBY(ODD BODY) 는 모션캡쳐 기술을 통해 인간의 신체를 메타버스에<br />
-        구현하기 위한 프로젝트이다.<br /><br />
-        블라블라
-      </FadeIn>
+      <Stack direction="column">
+        <Box>
+          <img
+            data-aos="fade-up"
+            data-aos-delay="500" 
+            src={image} 
+            className="App-logo" 
+            alt="logo" 
+          />
+        </Box>
+        <Stack 
+          alignItems="center"   
+          justifyContent="center"
+        >
+          <Box>
+            <h4 
+              data-aos="fade-up"
+              data-aos-delay="500" 
+            >
+              ODBY(ODD BODY) 는 모션캡쳐 기술을 통해 인간의 신체를 메타버스에<br />
+              구현하기 위한 프로젝트이다.<br /><br />
+              블라블라
+            </h4>
+          </Box>
+        </Stack>
+        </Stack>
     </div>
   );
 };
